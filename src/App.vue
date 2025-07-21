6e62b690549e7886c19ba97ffbfe73ab9bd46c77
Stexley <112969804@qq.com>
2025-07-21 17:52:39 +0800

    f

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div
+        style="height: 100%; box-shadow: var(--el-border-color-light) 0px 0px 10px"
+    >
+        <el-splitter>
+
+            <el-splitter-panel 
+                :size="64"
+                :min-size="50"
+                :max-size="50"
+                :resizable="false"
+            >
+                <div class="demo-panel">
+                    <Aside/>
+                </div>
+            </el-splitter-panel>
+
+            <el-splitter-panel size="250" min="200" max="350">
+                <div class="demo-panel">Function</div>
+            </el-splitter-panel>
+
+            <el-splitter-panel>
+                <div class="demo-panel">Main Interface</div>
+            </el-splitter-panel>
+
+            <el-splitter-panel size="0">
+                <el-splitter layout="vertical">
+
+                    <el-splitter-panel>
+                        <div class="demo-panel">Query</div>
+                    </el-splitter-panel>
+
+                    <el-splitter-panel>
+                        <div class="demo-panel">Translate</div>
+                    </el-splitter-panel>
+
+                </el-splitter>
+            </el-splitter-panel>
+
+        </el-splitter>
+    </div>
+</template>
+
+<script lang="ts">
+    import Aside from './components/aside.vue';
+
+    // Fix 'App'Div Height Data
+    function refuseScreenSize(){
+        (document.getElementById('app') as any).style.height = window.innerHeight + 'px';
+    };
+    refuseScreenSize();
+    window.addEventListener('resize',refuseScreenSize)
+
+    
+    export default {
+        name:'App',
+        components:{
+            Aside
+        }
+    }
+</script>
+
+<style scoped>
+    .demo-panel {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+</style>
+
+
---
+++ src/components/aside.vue
@@ -0,0 +1,110 @@
+<!-- <template>
+    <div id="AsideFrame" class="AsideFrame">
+        <el-radio-group v-model="tabPosition" style="margin-bottom: 30px">
+            <el-radio-button value="top">top</el-radio-button>
+            <el-radio-button value="right">right</el-radio-button>
+            <el-radio-button value="bottom">bottom</el-radio-button>
+            <el-radio-button value="left">left</el-radio-button>
+        </el-radio-group>
+
+        <el-tabs :tab-position="tabPosition" style="height: 200px" class="demo-tabs">
+            <el-tab-pane label="User">User</el-tab-pane>
+            <el-tab-pane label="Config">Config</el-tab-pane>
+            <el-tab-pane label="Role">Role</el-tab-pane>
+            <el-tab-pane label="Task">Task</el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Aside'
+    }
+</script>
+
+<script lang="ts" setup>
+    import { ref } from 'vue'
+    import type { TabsInstance } from 'element-plus'
+
+    const tabPosition = ref<TabsInstance['tabPosition']>('left')
+</script>
+
+<style>
+    .AsideFrame{
+        height: 100%;
+        width: 50px;
+        color: white;
+        border-right: 1.5px solid rgb(243, 237, 237)
+    }
+    .demo-tabs > .el-tabs__content {
+        padding: 32px;
+        color: #6b778c;
+        font-size: 32px;
+        font-weight: 600;
+    }
+    .el-tabs--right .el-tabs__content,
+    .el-tabs--left .el-tabs__content {
+        height: 100%;
+    }
+</style> -->
+
+<template>
+    <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        :collapse="isCollapse"
+        @open="handleOpen"
+        @close="handleClose"
+        :collapse-transition="false"
+    >
+        <el-menu-item index="1">
+            <el-icon><location /></el-icon>
+            <template #title>Navigator One</template>
+        </el-menu-item>
+        
+        <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>Navigator Two</template>
+        </el-menu-item>
+
+        <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <template #title>Navigator Three</template>
+        </el-menu-item>
+
+        <el-menu-item index="4">
+            <el-icon><setting /></el-icon>
+            <template #title>Navigator Four</template>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from 'vue'
+    import {
+        Document,
+        Menu as IconMenu,
+        Location,
+        Setting,
+    } from '@element-plus/icons-vue'
+
+    const isCollapse = ref(true)
+    const handleOpen = (key: string, keyPath: string[]) => {
+        console.log(key, keyPath)
+    }
+    const handleClose = (key: string, keyPath: string[]) => {
+        console.log(key, keyPath)
+    }
+</script>
+
+<style scoped>
+    .el-menu-item.is-active::after {
+        content: "";
+        position: absolute;
+        left: 0px;
+        top: 2px;
+        bottom: 2px;
+        width: 3px;
+        background: var(--el-color-primary);
+    }
+</style>
